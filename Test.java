5b089c56b60b2eaacd69ab7bf114445023d25647
MarioDiBiase1 <85687379+MarioDiBiase1@users.noreply.github.com>
2021-06-10 16:02:35 +0200

    Update Test.java

--- Test.java
+++ Test.java
@@ -7,7 +7,7 @@
 //Modifica da Raffaele
 public class Avvio {
     public static void main(String[] args) {
-        System.out.print("Hello world");            
+        System.out.print("Ciao Mondo");            
     }
 }
 
