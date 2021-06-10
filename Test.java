c3124b5c855bf66422e144092d8e44bc403d7e19
IamRaffaele <85687643+IamRaffaele@users.noreply.github.com>
2021-06-10 16:01:20 +0200

    Update Test.java

--- Test.java
+++ Test.java
@@ -4,6 +4,7 @@
 // E' passato Nicola (╯°□°)╯︵ ┻━┻
 //Salve a tutti by Antonio
 //Prova pseudo 2
+//Modifica da Raffaele
 public class Avvio {
     public static void main(String[] args) {
         System.out.print("Hello world");            
