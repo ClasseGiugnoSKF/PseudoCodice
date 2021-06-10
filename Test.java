b49d7e6cc8fb1c0b4e09f72631162343c177c6d3
Mirko Onorato <85618159+mirkoSkF@users.noreply.github.com>
2021-06-10 16:29:39 +0200

    Update Test.java

--- Test.java
+++ Test.java
@@ -9,7 +9,8 @@
 //prova Giovanni
 public class Avvio {
     public static void main(String[] args) {
-        System.out.print("Ciao Mondo");            
+        System.out.print("Ciao Mondo");  
+        //Saluti da Mirko!!!111!!! Yeeeh!!!
     }
 }
 
