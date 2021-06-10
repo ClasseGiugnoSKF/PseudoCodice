1c4d0fd1176509f98d9c9e754bc782e5bcc1a890
FabrizioMolino98 <85685811+FabrizioMolino98@users.noreply.github.com>
2021-06-10 15:53:16 +0200

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,6 +3,7 @@
 //Davide was here
 // E' passato Nicola (╯°□°)╯︵ ┻━┻
 //Salve a tutti by Antonio
+//Prova pseudo 2
 public class Avvio {
     public static void main(String[] args) {
         System.out.print("Hello world");            
